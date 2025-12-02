5047c7c5b639762fb940bc87d01d2b762040920f
Manoranjan2001-14 <1111manoranjan@gmail.com>
2025-12-02 19:55:37 +0530

    Inheritance code add

---
+++ Oops_concept/src/com/code/Main.java
@@ -0,0 +1,9 @@
+package com.code;
+
+public class Main {
+    public static void main(String[] args) {
+        Child ch = new Child();
+        ch.show();
+        ch.display();
+    }
+}
---
+++ Oops_concept/src/com/code/Parent.java
@@ -0,0 +1,13 @@
+package com.code;
+
+class Parent {
+    void show() {
+        System.out.println("This is Parent class");
+    }
+}
+
+class Child extends Parent {
+    void display() {
+        System.out.println("This is Child class");
+    }
+}
